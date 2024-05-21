278209be1cb16cbacbc37cf3397a2110dd835483
user <csgraycatdev@gmail.com>
2024-05-21 23:18:20 +0300

    Allow to type desired text via the address bar

--- ms_env/new_site/my_site/settings.py
+++ ms_env/new_site/my_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+	'line.apps.LineConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
